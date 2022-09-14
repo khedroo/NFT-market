a32c5584586998d25162875bef2df519e9a7d578
khedroo <ahmed0osama272@gmail.com>
2022-09-14 20:34:13 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,137 @@
+$('#slide1').owlCarousel({
+    loop: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+        dots: false
+      },
+      600: {
+        stagePadding: 100,
+        margin: 24,
+        nav: true,
+        items: 1,
+        dots: false
+      },
+      767: {
+        stagePadding: 180,
+        margin: 24,
+        nav: true,
+        items: 1,
+        dots: false
+      },
+      900: {
+        stagePadding: 250,
+        margin: 24,
+        nav: true,
+        items: 1,
+        dots: false
+      },
+      1100: {
+        stagePadding: 100,
+        margin: 24,
+        nav: true,
+        items: 2,
+        dots: false
+      },
+      1200: {
+        stagePadding: 200,
+        margin: 24,
+        nav: true,
+        items: 2,
+        dots: false
+      },
+      1500: {
+        stagePadding: 150,
+        margin: 24,
+        nav: true,
+        items: 3,
+        dots: false
+      },
+      1750: {
+        stagePadding: 264,
+        margin: 24,
+        nav: true,
+        items: 3,
+        dots: false
+      }
+    }
+  });
+$('#slide2').owlCarousel({
+    loop: true,
+    freeDrag: false,
+    responsive: {
+      0: {
+        items: 1.1,
+        margin: 10,
+        nav: true,
+        dots: false
+      },
+      767: {
+        items: 1.2,
+        margin: 16,
+        nav: true,
+        dots: false
+      },
+      1200: {
+        items: 1.5,
+        margin: 16,
+        nav: true,
+        dots: false
+      }
+    }
+  });
+$('#slide3').owlCarousel({
+    loop: true,
+    freeDrag: false,
+    responsive: {
+      0: {
+        items: 4,
+        stagePadding: 30,
+        nav: false,
+        dots: false
+      },
+      480: {
+        items: 5,
+        stagePadding: 50,
+        nav: false,
+        dots: false
+      },
+      650: {
+        items: 8,
+        stagePadding: 50,
+        nav: false,
+        dots: false
+      },
+      767: {
+        items: 9,
+        stagePadding: 50,
+        nav: false,
+        dots: false
+      },
+      900: {
+        items: 11,
+        stagePadding: 50,
+        nav: false,
+        dots: false
+      },
+      1200: {
+        items: 11,
+        stagePadding: 70,
+        nav: false,
+        dots: false
+      },
+      1400: {
+        items: 12,
+        stagePadding: 70,
+        nav: false,
+        dots: false
+      },
+      1600: {
+        items: 13,
+        stagePadding: 70,
+        nav: false,
+        dots: false
+      }
+    }
+  });
